fed2fe673ad8f643ff7e2958071128eeae9b4b76
Azi1403 <azam.digit@gmail.com>
2025-06-06 00:16:22 +0800

    j4

---
+++ string-course.py
@@ -0,0 +1,35 @@
+name="azam"
+s="i love python ,its my favorite language"
+text="iSsubClass"
+
+print(name.upper())
+print(text.lower())
+print(s.title())
+print(text.capitalize())
+print(s.count('python'))
+print(s.replace('python','english'))
+
+
+# Ncode=input("enter your national ID code:")
+# print(Ncode.isnumeric())
+
+name=input("your name:   ")
+print(name.isalpha())
+print(name.isalnum())
+
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[0])
+
+print(name.find('j'))
+
+#print(name.index('j'))
+print(name[::-1]) # چاپ معکوس کلمه یا جمله
+
+text='i love python'
+print(text[2:6]) # چاپ لاو
+print(text[6:1:-1]) # چاپ معکوس لاو
+
+
--- welcome.py
+++ welcome.py
@@ -1,5 +1,5 @@
-name=input("please write your name:")
-age=eval(input("your age?"))
+name = input("please write your name:")
+age = eval(input("your age?"))
 
 print(f"welcome{name} 10 years later your age is {age+10}")
 print(f"your age was{age}")
