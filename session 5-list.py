227d2436c7128dc7cf61d59eb4dd7e4c2447c8c3
Azi1403 <azam.digit@gmail.com>
2025-07-04 10:46:03 +0330

    ch

---
+++ session 5-list.py
@@ -0,0 +1,14 @@
+people = ['mehrnaz','azam','mehdi','matin']
+nums = [45,101,55,12.5]
+mix = ['sara',15,'azam',55.5]
+
+information = ['sara','hatami',33,1.64,True,['saman','sasan']]
+
+
+
+print(people[0]) #mehrnaz
+print(people[2]) #mehdi
+print(people[-1]) #matin
+
+people.append('ilia')
+print(people)
