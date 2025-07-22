08504564a9e5fd9b1ef914cd7feb1700eb52d0e6
Azi1403 <azam.digit@gmail.com>
2025-07-22 20:23:18 +0330

    scraper

---
+++ rq_scraper.py
@@ -0,0 +1,39 @@
+import requests
+import bs4
+response = requests.get("https://www.tgju.org/profile/price_dollar_rl")
+#print(response)
+#print(response.text)
+soup = bs4.BeautifulSoup(response.text,"html.parser")
+#print(soup)
+result_list = soup.find_all("span",attrs={"data-col":"info.last_trade.PDrCotVal"})
+price = result_list[0].text
+print(f"dollar roz: {price} rial")
+
+response = requests.get("https://www.tgju.org/profile/price_eur")
+soup = bs4.BeautifulSoup(response.text,"html.parser")
+result_list = soup.find_all("span",attrs={"data-col":"info.last_trade.PDrCotVal"})
+price = result_list[0].text
+print(f"euro roz: {price} rial")
+
+response = requests.get("https://www.tgju.org/profile/price_cny")
+soup = bs4.BeautifulSoup(response.text,"html.parser")
+result_list = soup.find_all("span",attrs={"data-col":"info.last_trade.PDrCotVal"})
+price = result_list[0].text
+print(f"cny roz: {price} rial")
+
+
+while True:
+    currency = input("currency: GBP,EUR,CNY,usd??").lower()
+    if currency == 'usd':
+        response = requests.get("https://www.tgju.org/profile/price_dollar_rl")
+    else:    
+        response = requests.get(f"https://www.tgju.org/profile/price_{currency}")
+
+
+    soup = bs4.BeautifulSoup(response.text,"html.parser")
+    result_list = soup.find_all("span",attrs={"data-col":"info.last_trade.PDrCotVal"})
+    price = result_list[0].text
+    print(f" {price} rial")
+    r=input("do you want to continue:Y/N?").lower()
+    if r=='n':
+        break
